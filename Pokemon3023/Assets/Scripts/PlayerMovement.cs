340e5fd3d1386d7df674774d860a821aa8966229
Illyria07 <amasumig@hotmail.com>
2020-10-07 01:48:34 -0400

    Tilemap Created
    Pokemon Route 1 duplicated for background

---
+++ Pokemon3023/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator anim;
+
+    [SerializeField]
+    float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementVector *= Time.deltaTime * speed;
+        transform.Translate(movementVector);
+    }
+
+    //if (Input.GetKey(KeyCode.UpArrow))
+    //    {
+    //        transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+    //        anim.SetBool("isWalking", true);
+    //        anim.SetInteger("playerDir", 0);
+    //    }
+
+}
