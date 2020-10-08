a96db1a018bd58fb09edfd070204e463ecb1cce6
Russell Brabers <brabers.russell@gmail.com>
2020-10-08 14:16:12 -0400

    Random Encounter
    - Created RandomEncounter script

---
+++ Pokemon3023/Assets/Scripts/RandomEncounter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class RandomEncounter : MonoBehaviour
+{
+    public GameObject player;
+    private Vector2 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void EncounterEvent()
+    {
+        int r = Random.Range(0, 100);
+        if (r <= 33)
+            Debug.Log("Random Encounter Occured!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            position = player.transform.position;
+            EncounterEvent();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            if ((Mathf.Abs(player.transform.position.x) - Mathf.Abs(position.x) >= 1) ||
+                (Mathf.Abs(player.transform.position.x) - Mathf.Abs(position.x) <= -1) ||
+                    (Mathf.Abs(player.transform.position.y) - Mathf.Abs(position.y) >= 1) ||
+                    (Mathf.Abs(player.transform.position.y) - Mathf.Abs(position.y) <= -1))
+            {
+                EncounterEvent();
+                position = player.transform.position;
+            }
+        }
+    }
+}
