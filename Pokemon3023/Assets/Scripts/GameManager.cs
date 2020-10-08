1ec262b8f120b36beb837eac2172d011a18ea844
Russell Brabers <brabers.russell@gmail.com>
2020-10-08 12:32:26 -0400

    Game Manager
    - Created Game Manager script
    - Added functionality to title scene button

---
+++ Pokemon3023/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
