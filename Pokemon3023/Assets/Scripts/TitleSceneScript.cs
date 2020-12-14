0270cf55c104e09173b3cb1e2310eeecf7e4b072
Illyria07 <amasumig@hotmail.com>
2020-12-14 02:51:53 -0500

    Footsteps and Title Music

--- Pokemon3023/Assets/Scripts/TitleSceneScript.cs
+++ Pokemon3023/Assets/Scripts/TitleSceneScript.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class TitleSceneScript : MonoBehaviour
 {
+    MusicManager musicM;
+
+    void Start()
+    {
+        musicM = FindObjectOfType<MusicManager>();
+    }
+
     public void StartGame()
     {
         GameManager.Instance().ChangeScene(1);
+        musicM.onEncounterEndHandler();
     }
 }
