7c01c050885c28cac3e93677c2ce6c2078fb3d5f
Russell Brabers <brabers.russell@gmail.com>
2020-12-13 23:56:58 -0500

    Main Screen UI
    - Added buttons to main screen

---
+++ Pokemon3023/Assets/Scripts/MainScreenUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScreenUI : MonoBehaviour
+{
+    public void OnSaveClicked()
+    {
+        GameStats.Instance().SaveStats();
+    }
+
+    public void OnLoadClicked()
+    {
+        GameStats.Instance().LoadStats();
+    }
+
+    public void OnAbilitiesClicked()
+    {
+
+    }
+}
